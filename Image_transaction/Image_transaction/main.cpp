a128db979479fda27ef0f659706f52a657531e5a
Luckydipper <qhrqufdlek@naver.com>
2021-07-06 07:26:27 +0900

    [add] - opencv default

---
+++ Image_transaction/Image_transaction/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main(int ac, char** av) {
+
+    Mat img = imread("web.jpg"); //자신이 저장시킨 이미지 이름이 입력되어야 함, 확장자까지
+
+    imshow("img", img);
+    waitKey(0);
+
+    return 0;
+}
